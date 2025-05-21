d3b17a19e6de69e155210b5b432b75e308eb0be3
RHoro <notender_boy@ukr.net>
2025-05-21 19:35:02 +0300

    refactor: task #1

--- public/js/ImagesResolver.js
+++ public/js/ImagesResolver.js
@@ -1,25 +1,41 @@
 window.ImagesResolver = (function () {
   class ImagesResolver {
-    constructor() {
-
-    }
+    constructor() {}
 
     search(query) {
-      return {
-        query: 'example',
-        images: [
-          {
-            id: 1,
-            url: '/img/mammal-3162194_640.jpg',
-            tags: 'panda'
-          },
-          {
-            id: 2,
-            url: '/img/panda-659186_640.png',
-            tags: 'panda'
-          }
-        ]
-      };
+      if (!query) return { query, images: [] };
+
+      const lowercasedQuery = query.toLowerCase();
+
+      const images = window.localDB
+        .filter((item) => {
+          const itemTags = item.tags.split(", ");
+
+          return itemTags.includes(lowercasedQuery);
+        })
+        .map(({ id, previewURL, tags }) => ({
+          id,
+          url: previewURL,
+          tags,
+        }));
+
+      return { query, images };
+
+      // return {
+      //   query: "example",
+      //   images: [
+      //     {
+      //       id: 1,
+      //       url: "/img/mammal-3162194_640.jpg",
+      //       tags: "panda",
+      //     },
+      //     {
+      //       id: 2,
+      //       url: "/img/panda-659186_640.png",
+      //       tags: "panda",
+      //     },
+      //   ],
+      // };
     }
   }
 
